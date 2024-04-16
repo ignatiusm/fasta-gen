583efd38914866e00b082b4c7096aa83a35df2db
ignatiusm <ignatius.menzies@gmail.com>
2024-04-16 16:30:54 +1200

    generates random string

--- src/fasta_gen/core.clj
+++ src/fasta_gen/core.clj
@@ -14,7 +14,8 @@
       (float (/ @seed m)))))
       
 ;; From https://en.wikipedia.org/wiki/Nucleic_acid_notation
-;;Full spec includes [ A C G T U W S M K R Y B D H V N]
+;;Full spec includes 
+;(def n-bases ["A" "C" "G" "T" "U" "W" "S" "M" "K" "R" "Y" "B" "D" "H" "V" "N"])
 (def n-bases ["A" "C" "G" "T"])
 
 (defn random-base []
@@ -25,12 +26,15 @@
       (< r 0.75) (nth n-bases 2)
       :else (nth n-bases 3)
       )))
-    
-  
+      
+(defn base-sequence [n, seq]
+  (if (= n 0)
+    seq
+    (base-sequence (dec n), (str seq (random-base)))))
       
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; Call the random function without an argument
   ;; This will generate a random number
-  (println "Random base:" (random-base)))
+  (println "Sequence:" (base-sequence 10 "")))
